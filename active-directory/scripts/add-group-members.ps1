ba448d0a3dd9929860ccd7f8e2376f4b8deb239d
trushilpatel29 <patel.trushil@gmail.com>
2026-05-19 18:47:21 +0100

    add-group-members.ps1

---
+++ active-directory/scripts/add-group-members.ps1
@@ -0,0 +1,4 @@
+Add-ADGroupMember -Identity "IT-Admins" -Members "sbennett","mreed","dhughes","acarter"
+Add-ADGroupMember -Identity "HR-Staff" -Members "oturner","nbrooks","ecollins","cramirez"
+Add-ADGroupMember -Identity "Finance-Staff" -Members "gmitchell","ifoster","jprice","lsimmons"
+Add-ADGroupMember -Identity "Managers" -Members "amorales","cbailey","hcooper","vhayes"
