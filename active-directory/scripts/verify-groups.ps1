60ea5a4a516b50f38f287378d9e6200f08b47fd4
trushilpatel29 <patel.trushil@gmail.com>
2026-05-19 19:05:19 +0100

    verify-groups.ps1

--- active-directory/scripts/verify-groups.ps1
+++ active-directory/scripts/verify-groups.ps1
@@ -1,1 +1,18 @@
+# Verify IT-Admins members
+Write-Host "IT-Admins Members:" -ForegroundColor Cyan
+Get-ADGroupMember -Identity "IT-Admins" | Select Name
 
+
+# Verify HR-Staff members
+Write-Host "HR-Staff Members:" -ForegroundColor Cyan
+Get-ADGroupMember -Identity "HR-Staff" | Select Name
+
+
+# Verify Finance-Staff members
+Write-Host "Finance-Staff Members:" -ForegroundColor Cyan
+Get-ADGroupMember -Identity "Finance-Staff" | Select Name
+
+
+# Verify Managers members
+Write-Host "Managers Members:" -ForegroundColor Cyan
+Get-ADGroupMember -Identity "Managers" | Select Name
